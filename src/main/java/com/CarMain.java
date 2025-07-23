5c5f5352ebe6e5223dfd9f500979bf80a1be335e
Kevin-Xiao0704 <blueeyebee@gmail.com>
2025-07-23 19:57:26 +0800

    00

--- src/main/java/com/CarMain.java
+++ src/main/java/com/CarMain.java
@@ -1,12 +1,13 @@
 package com;
 
 import com.model.Toyota;
+import com.model.ToyotaEngine;
 
 /** */
 public class CarMain {
 
 	public static void main(String[] args) {
-		Toyota toyota = new Toyota();
+		Toyota toyota = new Toyota(new ToyotaEngine());
 		toyota.move();
 
 	}
