419747b7eb33e6f7328617fafa757427d55d9b94
Kevin-Xiao0704 <blueeyebee@gmail.com>
2025-08-20 21:49:38 +0800

    8/20
    8/20

--- 08-Springboot-jpa1/src/main/java/com/course/repository/TodoRepository.java
+++ 08-Springboot-jpa1/src/main/java/com/course/repository/TodoRepository.java
@@ -4,7 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.course.entity.TodoEntity;
 
@@ -25,4 +29,22 @@ public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
 	List<TodoEntity> findByTitleOrderByDueDateDesc(String title);
 	
 	List<TodoEntity> findAllByOrderByDueDate();
+	
+	List<TodoEntity> findByTitleContainingOrderByDueDateDesc(String title);
+	
+	// JPQL
+	@Query("select t from TodoEntity t where t.title like ?1 order by t.dueDate desc")
+	List<TodoEntity> findByCondition(String title);
+	
+	// @Query("select t from TodoEntity t where t.title like ?1  order by t.dueDate desc")
+	@Query(nativeQuery = true, value = "select * from todo t where t.title like ? order by t.due_date desc")
+	List<TodoEntity> findByConditionNative(String title);
+	
+	@Query("select t from TodoEntity t where t.status = :st and t.title like :tt ")
+	List<TodoEntity> findByStatusAndTitle(@Param("st") Integer status, @Param("tt") String title);
+
+	@Transactional
+	@Modifying
+	@Query(nativeQuery = true, value = "update todo set title = ? where id = ? ")
+	void updateTodoTitle(String title, Long id);
 }
